6c727032856120ee8faa678e962ff13e7f49f587
bryan sine <bryansine1738@gmail.com>
2025-04-27 23:41:11 +0300

    update

--- drf/urls.py
+++ drf/urls.py
@@ -10,6 +10,17 @@
 #     path('', include(router.urls)),
 # ]
 
+# from django.contrib import admin
+# from django.urls import path, include
+# from rest_framework import routers
+# from drf.views import ItemViewSet
+
+# router = routers.DefaultRouter()
+# router.register(r'items', ItemViewSet)
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
