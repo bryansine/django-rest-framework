92d0b6ca579892a538413f77926e1842a0cd4ae0
bryan sine <bryansine1738@gmail.com>
2025-04-28 01:31:50 +0300

    update

--- drf/urls.py
+++ drf/urls.py
@@ -1,6 +1,6 @@
 # from django.contrib import admin
 # from django.urls import path, include
-# from drf.views import ItemViewSet
+
 
 from django.contrib import admin
 from django.urls import path, include
