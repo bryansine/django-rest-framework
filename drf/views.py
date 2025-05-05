304c6a2eca2eeb24f30e87a80f0540b05a5fb04c
bryan sine <bryansine1738@gmail.com>
2025-05-05 16:46:19 +0300

    IsAdmin & IsReadOnly

--- drf/views.py
+++ drf/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminOrReadOnly, AllowAny
+# from rest_framework.permissions import IsAuthenticated, IsAdminOrReadOnly, AllowAny
+from rest_framework.permissions import IsAdminUser
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -7,5 +8,5 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [AllowAny, IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
     #permission_classes = [AllowAny, IsAuthenticated, IsAdminOrReadOnly]
