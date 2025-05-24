fc1a989b00b9af06f0bee74fae9bba3e68090e2e
bryan sine <bryansine1738@gmail.com>
2025-05-24 17:36:16 +0300

    updates

--- restframework/settings.py
+++ restframework/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-# Build paths inside the project like this:  /.
+# Build paths inside the project like :  /.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
