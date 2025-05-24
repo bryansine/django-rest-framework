86b2f28a44b602e9d0092f73467b3b1be6543a38
bryan sine <bryansine1738@gmail.com>
2025-05-24 17:35:47 +0300

    updates

--- restframework/settings.py
+++ restframework/settings.py
@@ -17,7 +17,6 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
