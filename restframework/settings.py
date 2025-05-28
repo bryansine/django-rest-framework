469710fb10945d150ae637b6ba0be94269ce27f0
bryan sine <bryansine1738@gmail.com>
2025-05-29 00:00:05 +0300

    updates

--- restframework/settings.py
+++ restframework/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en//ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
