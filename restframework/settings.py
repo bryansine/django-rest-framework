b7756969831e707450404eecb83790c8aba27b15
bryan sine <bryansine1738@gmail.com>
2025-06-01 23:34:25 +0300

    updates

--- restframework/settings.py
+++ restframework/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
