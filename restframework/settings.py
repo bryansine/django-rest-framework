e82eb1f5de54082d6ef48b5f242278e9e04cc7cf
bryan sine <bryansine1738@gmail.com>
2025-06-13 16:38:11 +0300

    token expiration

--- restframework/settings.py
+++ restframework/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript)
-# https://docs.djangoproject.com/en/5.2/
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
