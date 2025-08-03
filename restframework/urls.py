61b26750486846cf2c1b7db98f1884eef0e48455
bryan sine <bryansine1738@gmail.com>
2025-08-03 20:06:33 +0300

    updates

--- restframework/urls.py
+++ restframework/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 
 # from django.contrib import admin
+# from django.contrib import admin
+
 # from django.urls import path, include
 # from rest_framework.authtoken.views import obtain_auth_token
 
