59d4d76fdfcf2fc791698cd57bd2ad527d6bd3bc
bryansine <bryansine1738@gmail.com>
2026-01-22 21:45:51 +0300

    updates

--- restframework/urls.py
+++ restframework/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
+
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('drf.urls')),
