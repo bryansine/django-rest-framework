2545b65d5e537519ebb0de49058a3542b0e26c2d
bryansine <bryansine1738@gmail.com>
2026-02-14 16:16:18 +0300

    updates

--- restframework/urls.py
+++ restframework/urls.py
@@ -14,18 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-# from rest_framework.authtoken.views import obtain_auth_token
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('drf.urls')),
     path('api/auth/', include('knox.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+        # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
     
     
     
