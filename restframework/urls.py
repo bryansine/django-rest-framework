0da63faaca78d7905bf987029434e91b2b11474a
bryan sine <98818309+bryansine@users.noreply.github.com>
2026-03-28 22:52:55 +0300

    Update viewsets
    Comment out the import statement for 'include' from django.urls.

--- restframework/urls.py
+++ restframework/urls.py
@@ -9,7 +9,7 @@ Function views
 """
 
 from django.contrib import admin
-from django.urls import path, include
+#from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
