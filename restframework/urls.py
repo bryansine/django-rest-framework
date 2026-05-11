6d20f9321f467301a46a93b1bca3f890b4262ae2
bryansine <bryansine1738@gmail.com>
2026-05-11 15:28:06 +0300

    updates on Routers

--- restframework/urls.py
+++ restframework/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('api/', include('drf.urls')),
     path('api/auth/', include('knox.urls')),
     path('api/auth/', include('knox.ur?ls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 
 ]
