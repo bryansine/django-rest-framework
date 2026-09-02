0c18be779eba6c3687f6bb8a451423af192ef8b8
bryansine <bryansine1738@gmail.com>
2026-09-02 18:36:23 +0300

    code refinement

--- restframework/urls.py
+++ restframework/urls.py
@@ -9,7 +9,7 @@ Function views
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
